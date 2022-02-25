f673474eb9f1fb3fe0e7e87585bcb3007ffbcb3d
Mubashir Syed <msyed26@hawk.iit.edu>
2022-02-25 02:57:53 -0600

    add javascript code to evaluate condition of radio buttons

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+if (document.getElementById("pay").clicked == true)
+{
+  alert("button clicked");
+
+  var state = document.getElementById("state").value;
+  var city = document.getElementById("state").value;
+  var zip = document.getElementById("zip").value;
+  var adress = document.getElementById("addy").value;
+  alert("button clicked");
+}
+
+if (document.getElementById("same").clicked == true){
+  document.getElementById("state-2").value == state;
+
+
+}
