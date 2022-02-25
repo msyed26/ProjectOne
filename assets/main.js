e2b0c23458dbf56a5ded415f8f1914614cf94dfb
Mubashir Syed <msyed26@hawk.iit.edu>
2022-02-25 15:49:25 -0600

    HTML and CSS validation errrors corrected

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,19 @@
 'use strict';
+//buttons assigned to variable
+var button = document.getElementById("pay");
+var button1 = document.getElementById("same");
 
-
-if (document.getElementById("pay").clicked == true)
-{
-  alert("button clicked");
-
+//button function to assign form field values to variables
+button.onclick = function () {
   var state = document.getElementById("state").value;
   var city = document.getElementById("state").value;
   var zip = document.getElementById("zip").value;
   var adress = document.getElementById("addy").value;
-  alert("button clicked");
 }
-
-if (document.getElementById("same").clicked == true){
-  document.getElementById("state-2").value == state;
+//button function for same adress as shipping
+button1.onclick = function () {
+  document.getElementById("state-2").value = state.value;
+  alert("button clicked");
 
 
 }
