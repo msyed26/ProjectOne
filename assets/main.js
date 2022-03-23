e61861ada87df1ece8556497f24438ecc1a3c595
Mubashir Syed <msyed26@hawk.iit.edu>
2022-03-23 17:50:32 +0300

    add javascript form hide function

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,24 @@
 'use strict';
-//buttons assigned to variable
-var button = document.getElementById("pay");
-var button1 = document.getElementById("same");
+// //buttons assigned to variable
+// var button = document.getElementById("pay");
+// var button1 = document.querySelector("same");
 
-//button function to assign form field values to variables
-button.onclick = function () {
-  var state = document.getElementById("state").value;
-  var city = document.getElementById("state").value;
-  var zip = document.getElementById("zip").value;
-  var adress = document.getElementById("addy").value;
-}
+// //button function to assign form field values to variables
+// button.onclick = function () {
+//   var state = document.getElementById("state").value;
+//   var city = document.getElementById("state").value;
+//   var zip = document.getElementById("zip").value;
+//   var adress = document.getElementById("addy").value;
+// }
 //button function for same adress as shipping
-button1.onclick = function () {
-  document.getElementById("state-2").value = state.value;
-  alert("button clicked")
+function showOrHideBillInfo() {
+  if(document.getElementById("same").checked) {
+    document.getElementById("bil").style.display="none";
+
+  }
+  else {
+    document.getElementById("bil").style.display="block";
+  }
 }
+//Listen and call function when using same adress
+document.querySelector
